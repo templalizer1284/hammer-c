3a7cf08efa1a4ea21870b9942eb196c0e7584d54
aca <aca@acas-MacBook-Pro.local>
2025-04-09 22:14:27 +0200

    Updated README, parsing update.

--- hammerc.h
+++ hammerc.h
@@ -1,3 +1,8 @@
+/*
+ * This is stupid C code generator from Hammer-C templates. It is not supposed to have
+ * any safety/type checking. If anything goes wrong your compiler will tell you.
+ * */
+
 #ifndef HAMMERC_C
 #define HAMMERC_C
 
@@ -14,15 +19,34 @@
 #define HC_DECL static inline
 #define MAX_INPUT 63
 #define MAX_SOURCE 65500
-#define ff fscanf(fp, "%63s", tmp);
+#define ff fscanf(glob.fp, "%63s", glob.tmp)
+#define cmp(x) strcmp(glob.tmp, Keywords[x][0])
 
 // ddecl
 typedef uint8_t u8;
 typedef struct hc_Source hc_Source;
 typedef struct hc_Globals hc_Globals;
 
+// ddef
+enum KEYWORDS {
+	CLASS,
+	PROPERTIES,
+	METHODS,
+	OBJECTS,
+	THIS,
+	STRING,
+	DEFINE,
+	IMPORT,
+	
+	END,
+	NUM_OF_KEYWORDS
+};
+
 struct hc_Source {
-	int x; // todo
+	char class_name[MAX_INPUT];
+	char *properties[MAX_INPUT][MAX_INPUT];
+
+	u8 properties_count;
 };
 
 struct hc_Globals {
@@ -33,15 +57,29 @@ struct hc_Globals {
 
 // fdecl
 HC_DECL u8 hc_Compile(const char *restrict);
-HC_DECL u8 hc_ParseClass(void);
 
-// globals
+HC_DECL u8 hc_ParseClass(void);
+HC_DECL u8 hc_ParseProperties(void);
+
+HC_DECL u8 hc_GenerateC(void);
+
+// globs
 static hc_Globals glob = {
 				.fp = NULL,
 				.tmp = ""
 						};
 
-// ddef
+char *Keywords[NUM_OF_KEYWORDS][MAX_INPUT] = {
+	{ "@class" },
+	{ "@properties" },
+	{ "@methods" },
+	{ "@objects" },
+	{ "@this" },
+	{ "@string" },
+	{ "@define" },
+	{ "@import" },
+	{ "@end" }
+};
 
 // fdef
 u8
@@ -60,8 +98,11 @@ hc_Compile(const char *restrict path) {
 				fprintf(stderr, "Class parsing error. Aborting.\n");
 				return 1;
 			}
-			
-			fclose(glob.fp);
+
+			if(hc_ParseProperties()) {
+				fprintf(stderr, "Properties parsing error. Aborting.\n");
+				return 1;
+			}
 		}
 	}
 
@@ -70,11 +111,53 @@ hc_Compile(const char *restrict path) {
 		return 1;
 	}
 
+	fclose(glob.fp);
+
 	return 0;
 }
 
 u8
 hc_ParseClass(void) {
+
+	while(ff == 1) {
+		if(cmp(CLASS) == 0) {
+			ff;
+			(void)snprintf(glob.src.class_name, sizeof(glob.src.class_name),
+			"%s", glob.tmp);
+			return 0;
+		}
+
+		else {
+			fprintf(stderr, "Beginning of Hammer-C file must start with %s declaration."
+			Keywords[CLASS][0] );
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+u8
+hc_ParseProperties(void) {
+
+	while(ff == 1) {
+		if(cmp(PROPERTIES) == 0) {
+			while(ff == 1) {
+				//xx
+			}
+		}
+
+		else {
+			fprintf(stderr, "After class declaration properties must be defined. Aborting.\n");
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+u8
+hc_GenerateC(void) {
 	return 0;
 }
 
